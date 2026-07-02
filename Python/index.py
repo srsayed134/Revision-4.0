54d099f2c7926b36ccc9afe68a590b2101a06650
srsayed134 <srsayed134@gmail.com>
2026-07-03 03:13:30 +0600

    Getting input and assignment operators

--- Python/index.py
+++ Python/index.py
@@ -24,7 +24,48 @@ print(sub_result)
 # Divition
 """
 div_result = 10/2
-print(div_result) #5.0
+print(div_result)  #5.0
 div_result2 = 10//2
-print(div_result2) #5
+print(div_result2)  #5
+"""
+#Exponentiation
+"""
+power_result = 2**3
+print(power_result) #8
+
+twototwntyseven = 2 ** 27 -1
+print(twototwntyseven)
+"""
+#Modulo
+"""
+mod = 38%10
+print(mod) #8
+"""
+
+# 03. Getting input 
+"""
+input_name = input("Input your name: ")
+print("Your name is", input_name)
+"""
+# 04. Assignment Operators
+"""
+x = 10
+x += 5
+print(x) 
+
+y = 20
+y -= 12 # y = y - 12
+print(y)
+
+z = 4
+z *= 10
+print(z) #40
+
+z1 = 9
+z1 /= 2
+print(z1) #z1 //= 4 this // will round it
+
+z2 = 5
+z2 **= 2
+print(z2) #25
 """
