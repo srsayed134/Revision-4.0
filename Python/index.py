fe96653cdd4d035518b5e8199221b1f830642eb8
srsayed134 <srsayed134@gmail.com>
2026-07-02 10:58:07 +0600

    Variable and arithmetic operations

---
+++ Python/index.py
@@ -0,0 +1,30 @@
+# 01. Variables
+
+"""
+age = 40
+age = 10
+user_password = 20
+print(user_password) > 20
+# print(age) > 10
+"""
+
+# 02. Arithmetic Operation
+
+# Addition
+"""
+sum_result = 5 + 3
+print(sum_result)
+"""
+#Subtraction
+"""
+sub_result = 29-9
+print(sub_result)
+"""
+# Multiplyin
+# Divition
+"""
+div_result = 10/2
+print(div_result) #5.0
+div_result2 = 10//2
+print(div_result2) #5
+"""
