f5d59d50c68ff86b10f46ff120cd0f80f12f62fa
srsayed134 <srsayed134@gmail.com>
2026-07-02 18:17:35 +0600

    Module Wraper

---
+++ Node-JS/index.js
@@ -0,0 +1,11 @@
+// 01. REPL 
+// By REPL we can command in terminal typing node > .help
+
+// 02. Module Wraper 
+/*
+(function (exports,require,module, __filename, __dirname){
+    console.log("Hello from IIFE")
+})();
+*/
+// console.log(__filename); //c:\Users\Sayed\Desktop\Revision 4.0 (Final)\Node-JS\index.js
+// console.log(__dirname); //c:\Users\Sayed\Desktop\Revision 4.0 (Final)\Node-JS
