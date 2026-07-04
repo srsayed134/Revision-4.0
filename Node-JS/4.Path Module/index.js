aa04e191035dc03c1cb6d4bdc85c9051c4912917
srsayed134 <srsayed134@gmail.com>
2026-07-04 12:00:24 +0600

    path module in node js

---
+++ Node-JS/4.Path Module/index.js
@@ -0,0 +1,10 @@
+import path from "path"
+
+// console.log(path.basename("c:/nodejs/index.js")) //index.js
+// console.log(path.basename("c:/nodejs/index.js", ".js")) //index
+// console.log(path.dirname("c:/nodejs/courses/app.js")) //c:/nodejs/courses
+// console.log(path.extname("c:/nodejs/courses/app.pug")) //.pug
+// console.log(path.join('c:', "huxn-webdeb", "courses", "redux-toolkit")) //c:\huxn-webdeb\courses\redux-toolkit
+
+//Go up a level
+// console.log(path.join('c:', "huxn-webdeb", "courses", "redux-toolkit", "..")) //c:\huxn-webdeb\courses
