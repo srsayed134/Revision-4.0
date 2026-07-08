8d7ac35b99f33dc358181903721fb198d38c3d81
srsayed134 <srsayed134@gmail.com>
2026-07-08 12:58:15 +0600

    Create custom array method push,pop,get,shift,unshift,delete

--- DSA/index.js
+++ DSA/index.js
@@ -71,3 +71,76 @@ function findPairs(arr){
 const numbers = [1,2,3,4,5];
 findPairs(numbers)
 */
+
+//O(log n) +++++++++++++++++
+//O(log n) time complexity refers to an algorithms runtime that grow logarithmically with the size of the input (represented by n).In simpler terms, as the input size increases, the time it takes for the algorithm to run increases slowly
+
+//DSA Arrays ++++++++++++++++++
+//Data structure array is an ordered collection of elements that can be accessed using a numerical index
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {}
+    }
+    push(item){
+        this.data[this.length] = item;
+        this.length++
+        return this.length
+    }
+    get(index){
+       let find = this.data[index]
+       return find
+    }
+    pop(){
+       const lastItem = this.data[this.length -1]
+       delete this.data[this.length - 1]
+       this.length --
+       return lastItem
+    }
+    shift(){
+        let firstItem = this.data[0]
+        //Reindexing
+        for(let i = 0; i < this.length; i++){
+            this.data[i] = this.data[i + 1]
+        }
+        //Delete last length
+        delete this.data[this.length - 1] //This last data should have removed because last length is created with out data
+        this.length--
+        return firstItem
+
+    }
+    delete(index){
+        let item = this.data[index];
+        for(let i = index; i < this.length - 1; i++){  //let i = index; i < this.length - 1; i++ and let i = index; i < this.length; i++ will give same result but -1 remove one unneccessary assignment
+
+            this.data[i] = this.data[i + 1] 
+        }
+        delete this.data[this.length - 1]
+        this.length --
+        return item
+    }
+    unshift(item){
+        this.length++
+        for(let i = this.length - 1; i > 0; i--){
+            this.data[i] = this.data[i - 1]
+        }
+        this.data[0] = item;
+        return this.length
+    }
+}
+
+const myNewArray = new MyArray()
+myNewArray.push("Mango")
+myNewArray.push("Banana")
+myNewArray.push("Orange")
+myNewArray.push("Pineapple")
+// console.log(myNewArray)
+// console.log(myNewArray.get(2))
+// console.log(myNewArray.pop())
+// console.log(myNewArray)
+// console.log(myNewArray.shift())
+// console.log(myNewArray)
+// myNewArray.delete(2)
+console.log(myNewArray)
+myNewArray.unshift("Watermelon")
+console.log(myNewArray)
